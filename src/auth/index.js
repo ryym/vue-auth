c00dbf293d734172f71d69d16c7b1113a4015a1b
ryym <ryym.64@gmail.com>
2016-01-16 23:24:59 +0900

    auth: Add login method

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,6 +2,7 @@
 
 import { router } from '../index';
 
+const LOGIN_URL = `${API_URL}/sessions/create`;
 const SIGNUP_URL = `${API_URL}/users/`;
 const TOKEN_KEY = 'vue-auth_id-token';
 
@@ -14,11 +15,24 @@ export default {
 
   _loggedOut() { this.user.authenticated = false; },
 
+  _storeToken(data) { localStorage.setItem(TOKEN_KEY, data.id_token); },
+
   signup(context, creds, redirect) {
     return context.$http
       .post(SIGNUP_URL, creds)
       .then(res => {
-        localStorage.setItem(TOKEN_KEY, res.data.id_token);
+        this._storeToken(res.data);
+        this._loggedIn();
+        redirect && router.go(redirect);
+      });
+  },
+
+  // Send a request to the login URL and save the returned JWT.
+  login(context, creds, redirect) {
+    return context.$http
+      .post(LOGIN_URL, creds)
+      .then(res => {
+        this._storeToken(res.data);
         this._loggedIn();
         redirect && router.go(redirect);
       });
