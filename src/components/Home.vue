0b4ed69aab6418d5bbb7cb51148eead3abd4909b
ryym <ryym.64@gmail.com>
2016-01-16 15:58:38 +0900

    home: Display random quote

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>Hello {{ name }}!</div>
+  <div class="col-sm-6 col-sm-offset-3">
+    <h1>Get a Free Chunk Norris Quote!</h1>
+    <button class="btn btn-primary" @click="getQuote">
+      Get a Quote
+    </button>
+    <div class="quote-area" v-if="quote">
+      <h2>
+        <blockquote>{{ quote }}</blockquote>
+      </h2>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   data() {
-    return { name: 'world' }
+    return { quote: '' }
+  },
+  methods: {
+    getQuote() {
+      this.$http
+        .get('http://localhost:3001/api/random-quote')
+        .then(res => this.quote = res.data);
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './components/App.vue';
 import Home from './components/Home.vue';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter();
 
