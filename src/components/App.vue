dc014c79ad979bc5d50d200d675f77abd3a455e7
ryym <ryym.64@gmail.com>
2016-01-17 16:52:59 +0900

    nav: Change links according to user state

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,11 +2,29 @@
   <nav class="navbar navbar-default">
     <div class="container">
       <ul class="nav navbar-nav">
-        <li><a v-link="'home'">Home</a></li>
-        <li><a v-link="'signup'">Signup</a></li>
-        <li><a v-link="'login'">Login</a></li>
-        <li><a v-link="'secretquote'">Seqret Quote</a></li>
-        <li><a v-link="'home'" @click="logout()">Logout</a></li>
+        <li>
+          <a v-link="'home'">Home</a>
+        </li>
+        <li>
+          <a v-link="'signup'" v-if="! user.loggedIn">
+            Signup
+          </a>
+        </li>
+        <li>
+          <a v-link="'login'" v-if="! user.loggedIn">
+            Login
+          </a>
+        </li>
+        <li>
+          <a v-link="'secretquote'" v-if="user.loggedIn">
+            Seqret Quote
+          </a>
+        </li>
+        <li>
+          <a v-link="'home'" v-if="user.loggedIn" @click="logout()">
+            Logout
+          </a>
+        </li>
       </ul>
     </div>
   </nav>
@@ -19,7 +37,18 @@
 
 import auth from '../auth';
 
+const user = {
+  loggedIn: false
+};
+
+auth.subscribe(state => {
+  user.loggedIn = state.authenticated;
+});
+
 export default {
+  data() {
+    return { user };
+  },
   methods: {
     logout() {
       auth.logout();
